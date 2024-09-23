c8bb5bc63a425982161d829e7bc5a5fc65cddef1
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2024-09-23 17:29:28 +0200

    Gradle build plugin

--- logic/build.gradle.kts
+++ logic/build.gradle.kts
@@ -1,48 +1,15 @@
 plugins {
-    java
-    jacoco
-    application
-    `java-library`
+    id("hu.bme.mit.ase.shingler.gradle.java")
 }
-
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
-}
-
-val picoCliVersion = "4.7.6"
-val slf4jVersion = "1.7.36"
-val log4jVersion = "2.23.1"
-val junitVersion = "5.10.0"
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
-
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-
     api(project(":lib"))
-}
 
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
+    implementation(libs.slf4j.api)
 
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
-}
+    runtimeOnly(libs.slf4j.log4j.impl)
 
-application {
-    mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
+    testImplementation(libs.junit.jupiter.core)
+
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
